be27d9251bd92acee5b3a140c41e341bde694a44
trlatimer <31456150+trlatimer@users.noreply.github.com>
2017-09-26 00:25:38 -0700

    Correct init inputs

--- media.py
+++ media.py
@@ -3,7 +3,7 @@ import webbrowser
 #class for movie objects
 class Movie():
     """ This class provides a way to store movie related information"""
-    def __init__(self)
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube)
       self.title = movie_title
       self.storyline = movie_storyline
       self.poster_image_url = poster_image
