f8a793b741649647387f4d67ef6bd39cf75a070a
Tyler Latimer <31456150+trlatimer@users.noreply.github.com>
2017-09-26 13:00:50 -0700

    Update media.py

--- media.py
+++ media.py
@@ -1,11 +1,12 @@
 import webbrowser
 
-#class for movie objects
+
+# class for movie objects
 class Movie():
     """ This class provides a way to store movie related information"""
-    def __init__(self, movie_title, movie_storyline, 
+    def __init__(self, movie_title, movie_storyline,
                  poster_image, trailer_youtube):
-        """ This is the constructor for the Movie class. Each movie needs 
+        """ This is the constructor for the Movie class. Each movie needs
                 a title, storyline, poster image, and movie trailer """
         self.title = movie_title
         self.storyline = movie_storyline
