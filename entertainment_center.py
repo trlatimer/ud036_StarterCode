cf620392607e49e016c014c4a40107f2f46ed82c
trlatimer <31456150+trlatimer@users.noreply.github.com>
2017-09-25 23:51:36 -0700

    Create entertainment_center.py
    Create entertainment_center.py to store movie information, add first movie

---
+++ entertainment_center.py
@@ -0,0 +1,9 @@
+import fresh_tomatoes
+import media
+
+american_sniper = media.Movie("American Sniper",
+                              "U.S. Navy SEAL Chris Kyle (Bradley Cooper) takes his sole mission -- protect his comrades -- to heart and becomes one of the most lethal snipers in American history. His pinpoint accuracy not only saves countless lives but also makes him a prime target of insurgents. Despite grave danger and his struggle to be a good husband and father to his family back in the States, Kyle serves four tours of duty in Iraq. However, when he finally returns home, he finds that he cannot leave the war behind.",
+                              "http://t2.gstatic.com/images?q=tbn:ANd9GcSCEJMtX2_SB-ZlvYpabgPb6dwI_bVSY-eVap6aSSSfFq5Ldmxa",
+                              "https://www.youtube.com/watch?v=99k3u9ay1gs")
+
+
