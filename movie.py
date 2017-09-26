7d302ec9b8ab759e9c7e9bd0fff92fbfdc02e5cc
trlatimer <31456150+trlatimer@users.noreply.github.com>
2017-09-25 23:45:52 -0700

    Create movie.py
    Create file containing class Movie

---
+++ movie.py
@@ -0,0 +1,10 @@
+import webbrowser
+
+#class for movie objects
+class Movie():
+    """ This class provides a way to store movie related information"""
+    def __init__(self)
+      self.title = movie_title
+      self.storyline = movie_storyline
+      self.poster_image_url = poster_image
+      self.trailer_youtube_url = trailer_youtube
